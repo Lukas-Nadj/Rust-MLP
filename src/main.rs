17194a8bb23f5d8ad50a87492ab17020b003cc5a
Squrtledude <nadjlukas@hotmail.com>
2023-12-30 03:13:52 +0100

    Fixed some initialisation
    Feeding forward on the same model twice should produce the same output, no reason to randomly generate an input.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 #[allow(unused_parens)]
-
-
 mod network;
-
-use network::{Layer, MultilayerPerceptron};
+use network::MultilayerPerceptron;
 
 
 fn main(){
     let mut model = MultilayerPerceptron::init( vec![10, 16, 16, 10]  );
+    model.feed();
+    println!("________________________________________________________________");
     model.feed();
     //println!("model: {:?}", model.activations[3]);
 }
--- src/network.rs
+++ src/network.rs
@@ -10,12 +10,14 @@ pub struct Layer {
 pub struct MultilayerPerceptron{
 	pub activations: Vec<Vec<f32>>,
 	pub layers: Vec<Layer>,
+    pub model_struct: Vec<i32>
 }
 
 impl MultilayerPerceptron {
 
 	pub fn init(  /*activation_function: F,*/ model_struct: Vec<i32> ) -> Self{	
         let mut activations: Vec<Vec<f32>> = vec![vec![]; model_struct.len()];
+        
         let mut layers: Vec<Layer> = (0..model_struct.len()).map(|_| Layer {
             weights: vec![vec![]],
             bias: vec![],
@@ -37,7 +39,7 @@ impl MultilayerPerceptron {
             }
         }
         
-        MultilayerPerceptron { activations, layers }
+        MultilayerPerceptron { activations, layers, model_struct}
     }
 
 
@@ -93,15 +95,44 @@ impl MultilayerPerceptron {
         output
     }
     
+    pub fn elementwise(&self, left: &Vec<f32>, right: &Vec<f32>, offset: f32) -> Vec<f32> {
+        let mut output: Vec<f32> = Vec::new();
+        output.resize((self.model_struct[self.model_struct.len()-1 as usize]).try_into().unwrap(), 0.0f32);
+        
+        for e in 0..left.len() {
+            output[e] = left[e] * (offset - right[e]);
+        }
+        output
+    }
+    
+    pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
+        where
+        T: Clone,
+        {
+        assert!(!v.is_empty());
+        (0..v[0].len())
+        .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
+        .collect()
+    }
+    
     pub fn feed(&mut self) {
-        let mut rng = rand::thread_rng();
-        self.activations[0] = Vec::new();
-        self.activations[0].resize_with(10, || rng.gen::<f32>());
         
         for l in 1..self.activations.len() as usize{
             self.activations[l] = MultilayerPerceptron::sigmoid(&MultilayerPerceptron::add(&MultilayerPerceptron::dot( &mut self.layers[l].weights, &mut self.activations[l-1]),  &self.layers[l].bias));
             println!("model: {:?}", self.activations[l]);
         }
+        
+    }
+    
+    pub fn backpropagate(&mut self, learning_rate: f64) {
+        /*
+        let activation_error: Vec<Vec<f32>> = Vec::new();
+        activation_error.resize(self.model_struct.length);
+        let dW = dot(dot(cost(), self.activations[l]),  ) 
+        
+        
+        probably wrong, but i'll figure it out when i actually write it.
+         */
     }
 
 }
