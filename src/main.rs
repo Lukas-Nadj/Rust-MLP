8230f64817de327b1289e13b9ebf3ba03e2c140b
Squrtledude <nadjlukas@hotmail.com>
2024-01-02 08:04:45 +0100

    I did not go to sleep last night!!
    I created some empty structs, very cool. not sure how to generalize the struct to different data sets. for now Data has a Vec<Vec<>> and it's your job to turn the csv into a Data object. and that double vec-array.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #[allow(unused_parens)]
 mod network;
-use network::MultilayerPerceptron;
+mod data;
+use network::MultilayerPerceptron; // specifies the network structure and method.
+use data::Data; //specifies the dimensions of the traning data.
+//use CSVloader //specifies the loading of data into memory.
+//use network::modelfs; // Methods for saving and loading the model.
 
 
 fn main(){
-    let mut model = MultilayerPerceptron::init( vec![10, 16, 16, 10]  );
+    
+    let data:Data = Data{data: vec![vec![0.0f64]], eval_data: vec![vec![0.0f64]], inputsize: 10, trainingsize: 2, evaluationsize: 1};
+    let mut model = MultilayerPerceptron::init( vec![784, 16, 16, 16, 16, 10], &data);
     
     //should print the same to the network
     model.feed();
-    println!("________________________________________________________________");
     model.feed();
-    
+
     //println!("model: {:?}", model.activations[3]);
 }
 
--- src/network.rs
+++ src/network.rs
@@ -1,22 +1,37 @@
 
 use rand::prelude::*;
+use crate::data::Data; 
+
 
 #[derive(Debug)]
 pub struct Layer { //:3
-    weights:  Vec<Vec<f32>>,
-    bias: Vec<f32>,
+    weights:  Vec<Vec<f64>>,
+    bias: Vec<f64>,
 }
 #[derive(Debug)]
-pub struct MultilayerPerceptron{
-	pub activations: Vec<Vec<f32>>,
+pub struct MultilayerPerceptron<'a>{
+    pub data: &'a Data,
+	pub activations: Vec<Vec<f64>>,
 	pub layers: Vec<Layer>, //could become a vec array of touples, or be defined inline without Layer struct. but for now that would be a headache.
     pub model_struct: Vec<i32> //could simplify some loops later
 }
 
-impl MultilayerPerceptron {
+pub enum BatchingType { 
+    MiniBatch(u32),
+    Batching,
+    Online
+}
 
-	pub fn init/*is convention ::new? probably but i don't care yet */(  /*activation_function: F,*/ model_struct: Vec<i32> ) -> Self{	
-        let mut activations: Vec<Vec<f32>> = vec![vec![]; model_struct.len()]; //initialize array size to model size
+impl<'a> MultilayerPerceptron<'a> {
+    pub fn load( model_fs:i32 , data:&'a Data ) /* -> Self */{
+        
+    }
+}
+
+impl<'a> MultilayerPerceptron<'a> {
+    
+	pub fn init( model_struct: Vec<i32>, data:&'a Data ) -> Self{	 //TODO: assert data.data has the correct dimensions. with input layer.
+        let mut activations: Vec<Vec<f64>> = vec![vec![]; model_struct.len()]; //initialize array size to model size
         
         let mut layers: Vec<Layer> = (0..model_struct.len()).map(|_| Layer { 
             weights: vec![vec![]],
@@ -26,46 +41,49 @@ impl MultilayerPerceptron {
         //initialize activations array
         let mut rng = rand::thread_rng();
         for (i, size) in model_struct.iter().enumerate() { 
-            activations[i].resize(*size as usize, 0.0f32); //insert closure that creates random values
+            activations[i].resize(*size as usize, 0.0f64); 
         }
         
         //initialize weights and biases with random floats.
         for (l, size) in model_struct.iter().enumerate() { 
             layers[l].weights.resize(*size as usize, Vec::new());
-            layers[l].bias.resize_with(*size as usize, || rng.gen::<f32>());
+            layers[l].bias.resize_with(*size as usize, || rng.gen::<f64>());
             for i in 0..*size as usize { 
                 if(l>0){
-                    layers[l].weights[i].resize_with(activations[l-1].len(), || rng.gen::<f32>());
+                    //i fucking love closures and resize_with !!!
+                    layers[l].weights[i].resize_with(activations[l-1].len(), || rng.gen::<f64>());  //insert closure that creates random values
                 }
             }
         }
-        //return a new model struct with these variables.
-        MultilayerPerceptron { activations, layers, model_struct}
+        //return a new model struct that takes ownership of these variables. //nope, we should just use the Self struct instead. now we can init an already initialized model without creating a new struct? a little useless.
+        Self {data, activations, layers, model_struct}
     }
 
 
-    fn cost(activations: &Vec<f32>, target: &Vec<f32>) {  
-        //not done, and not useful.
-        let mut sum = 0.0f32;
-        let mut cost: Vec<f32> = vec![0.0; target.len()];
+    pub fn cost(&self, activations: &Vec<f64>, target: &Vec<f64>) -> f64{  
+        //not done, and not useful. //how bout NOW benson
+        let mut sum = 0.0f64;
+        let mut cost: Vec<f64> = vec![0.0; target.len()];
         
-        for (index, err) in activations.iter().enumerate() {
+        for (index, _err) in activations.iter().enumerate() {
             cost[index] = (activations[index] - target[index]).powi(2);
             sum += (activations[index] - target[index]).powi(2);
         }
+        
+        sum
     }
 
-    pub fn dot(left: &Vec<Vec<f32>>, right: &Vec<f32>) -> Vec<f32> { //so far mostly for weights[][] dot activations[], not generalized.
-        //new vec<f32> for the result of the operation
-        let mut output:Vec<f32> = Vec::new();
-        output.resize(left.len(), 0.0f32);
+    pub fn dot(left: &Vec<Vec<f64>>, right: &Vec<f64>) -> Vec<f64> { //so far mostly for weights[][] dot activations[], not generalized.
+        //new vec<f64> for the result of the operation
+        let mut output:Vec<f64> = Vec::new();
+        output.resize(left.len(), 0.0f64);
         
         //get array len's to a temp variable so we don't need to retrieve it every iteration of the subsequent loop.
         let len = left.len();
         let len_inner = left[0].len();
         
         for row in 0..len {
-            let mut sum = 0.0f32;
+            let mut sum = 0.0f64;
             
             for i in 0..len_inner {
                 //Z += W*A[l], weighted sum for each neuron/row
@@ -77,14 +95,14 @@ impl MultilayerPerceptron {
         return output; //output would have been fine but i find this so much more readable from java ;-; might change later, doesn't really matter (i think?)
     } 
     
-    pub fn add(left: &Vec<f32>, right: &Vec<f32>) -> Vec<f32> {
+    pub fn add(left: &Vec<f64>, right: &Vec<f64>) -> Vec<f64> {
         //adding arrays 1 to 1 requires the same dimensions and length.
         assert_eq!(left.len(), right.len());
         assert_ne!(left.len(),  0, "Fuck");
         
         //init ouput array.
-        let mut output:Vec<f32> = Vec::new();
-        output.resize(left.len(), 0.0f32);
+        let mut output:Vec<f64> = Vec::new();
+        output.resize(left.len(), 0.0f64);
         
         if right.len() > 0 { //dunno why i wrote this, if it's dumb the compiler will remove it lol.
         for i in 0..right.len() {
@@ -95,23 +113,23 @@ impl MultilayerPerceptron {
         output
     }
     
-    pub fn sigmoid(z: &Vec<f32>) -> Vec<f32> { //applies activation function to an entire layer.
+    pub fn sigmoid(z: &Vec<f64>) -> Vec<f64> { //applies activation function to an entire layer.
         //init
-        let mut output:Vec<f32> = Vec::new();
-        output.resize(z.len(), 0.0f32);
+        let mut output:Vec<f64> = Vec::new();
+        output.resize(z.len(), 0.0f64);
         
         //Sigmoid activation
         for i in 0..z.len() {
-            output[i] = 1.0f32/(1.0f32+z[i].exp());
+            output[i] = 1.0f64/(1.0f64+z[i].exp());
         }
         
         output
     }
     
-    pub fn elementwise(&self, left: &Vec<f32>, right: &Vec<f32>, offset: f32) -> Vec<f32> {
+    pub fn elementwise(&self, left: &Vec<f64>, right: &Vec<f64>, offset: f64) -> Vec<f64> {
         //init
-        let mut output: Vec<f32> = Vec::new();
-        output.resize((self.model_struct[self.model_struct.len()-1 as usize]).try_into().unwrap(), 0.0f32);
+        let mut output: Vec<f64> = Vec::new();
+        output.resize((self.model_struct[self.model_struct.len()-1 as usize]).try_into().unwrap(), 0.0f64);
         
         //elementwise, and an offset for some of the derivatives later. (1-L*R), could also have made a seperate function to do this to the output, but this is simpler to me
         for e in 0..left.len() {
@@ -130,18 +148,72 @@ impl MultilayerPerceptron {
         .collect()
     }
     
-    pub fn feed(&mut self) {
+    pub fn feed(&mut self) -> f64{
         
         //matrix abstraction ftw.
         for l in 1..self.activations.len() as usize{
             // Z = σ(W*A[l-1]+B), basically takes the sigmoid of (weighted sum's + biases)
             self.activations[l] = MultilayerPerceptron::sigmoid(&MultilayerPerceptron::add(&MultilayerPerceptron::dot( &mut self.layers[l].weights, &mut self.activations[l-1]),  &self.layers[l].bias));
-            println!("model: {:?}", self.activations[l]); //should maybe also return result of feeding forward, for evaluation or visualization later.
+            
         }
+        
+        println!("model: {:.4?}", self.activations[self.activations.len()-1]); //should maybe also return result of feeding forward, for evaluation or visualization later.
+        return self.cost(&self.activations[self.activations.len()-1], &vec![0.0f64, 1.0f64, 0.0f64, 0.0f64, 0.0f64, 0.0f64, 0.0f64, 0.0f64, 0.0f64, 0.0f64]);
         
     }
     
-    pub fn backpropagate(&mut self, learning_rate: f64) {
+    pub fn backpropagate(&mut self, learning_rate: f64, epoch: f64, batching: BatchingType) {
+        
+        let error:Vec<f64> = Vec::new();
+        let batchsize:i32 = 1;
+        let cost: f64;
+        for e in 0..epoch as usize {
+            println!("Epoch: {}", e);
+            //forward propagation
+            match batching {
+                BatchingType::MiniBatch(size) => {
+                    //error.resize_with(self.data.data.len(), f)
+                    //iterate over slice with `size` iterate over each
+                }
+                BatchingType::Batching => {
+                    // batchsize = &mut self.data.data.len();
+                    /* 
+                    
+                    let mut sum:f64 = 0.0f64;
+                    
+                    for err in &feed result { 
+                    sum += err;
+                    }
+                    
+                    cost = sum/error.len() as f64;
+                    
+                    
+                    */
+                }
+                
+                _ => {
+                    
+                }
+            }
+        
+            //final cost
+            
+        
+            //backpropagation
+            /*
+            for cost in 0..erro.iter.len().whatever { //iterate over each batch
+            let mut prevlayer:f64 = cost.clone(); //variable for dC/da(l-1), cost with respect to the previous layer
+            for i in &mut self.data.data.len()..0 {
+                //weights -=
+                //bias -=
+                //prevlayer =
+            }
+            
+             */
+            
+        }
+        
+        
         /*
         let activation_error: Vec<Vec<f32>> = Vec::new();
         activation_error.resize(self.model_struct.length);
